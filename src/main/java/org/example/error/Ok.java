6ea185765fd0653cd08256f422631526ca90a0ac
IroncladLandship <jack.huffstutler@gmail.com>
2022-09-22 20:18:46 -0500

    Added the Ok implementation of Result

---
+++ src/main/java/org/example/error/Ok.java
@@ -0,0 +1,29 @@
+package org.example.error;
+
+public class Ok<T> implements Result<T> {
+    private final T value;
+
+    public Ok(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean isOk() {
+        return true;
+    }
+
+    @Override
+    public boolean isErr() {
+        return false;
+    }
+
+    @Override
+    public T unwrap() throws UnwrapOnErrException {
+        return value;
+    }
+
+    @Override
+    public Exception unwrapErr() throws UnwrapErrOnOkException {
+        throw new UnwrapErrOnOkException();
+    }
+}
